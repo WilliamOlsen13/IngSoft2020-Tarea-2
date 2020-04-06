d71e78a15f816032cb63dda6b9f979cb3f9f52d4
William <wolsen13@gmail.com>
2020-04-06 00:42:32 -0300

    Commit A - Atributos de la clase Usuario

---
+++ usuario.java
@@ -0,0 +1,10 @@
+public class Usuario{
+	
+	// ATRIBUTOS
+	private String nombre;
+	private String cedula;
+	private String password;
+	private int edad;
+	private int cantidadDeHijos;
+	private int saldo;
+}
